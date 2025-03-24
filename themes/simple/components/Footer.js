960500b19c2419c046bdef61631ba1558f6a8160
allencai90 <cyychad@gmail.com>
2025-03-24 14:47:45 +0800

    Update Footer.js

--- themes/simple/components/Footer.js
+++ themes/simple/components/Footer.js
@@ -37,9 +37,9 @@ export default function Footer(props) {
           <span className='no-underline ml-4'>
             Powered by
             <a
-              href='https://github.com/tangly1024/NotionNext'
+              href='https://barusummit.vercel.app/'
               className=' hover:underline'>
-              NotionNext {siteConfig('VERSION')}
+              BARU SUMMIT {siteConfig('VERSION')}
             </a>
           </span>
         </div>
