2449b2f8d793e24efbca41fdcf74061e20d70050
allencai90 <cyychad@gmail.com>
2026-02-18 19:14:06 +0800

    Update link in PoweredBy component

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -9,7 +9,7 @@ export default function PoweredBy(props) {
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
-        href='https://github.com/tangly1024/NotionNext'
+        href='https://www.uslogs.com/'
         className='underline justify-start'>
         BARU LOGISTIC {siteConfig('2026')}
       </a>
