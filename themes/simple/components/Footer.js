be15d4e617321708779a2aad3063c464faa24453
allencai90 <cyychad@gmail.com>
2026-02-18 20:09:40 +0800

    Update Footer.js

--- themes/simple/components/Footer.js
+++ themes/simple/components/Footer.js
@@ -37,7 +37,7 @@ export default function Footer(props) {
           <span className='no-underline ml-4'>
             Powered by
             <a
-              href='https://github.com/tangly1024/NotionNext'
+              href='https://uslogs.com'
               className=' hover:underline'>
               BARU LOGISTIC {siteConfig('2026')}
             </a>
