cf6b8f1f111c9e727c64aff26758be1a886e3e9b
allencai90 <cyychad@gmail.com>
2026-02-18 19:22:41 +0800

    Update Footer.js

--- themes/simple/components/Footer.js
+++ themes/simple/components/Footer.js
@@ -39,7 +39,7 @@ export default function Footer(props) {
             <a
               href='https://github.com/tangly1024/NotionNext'
               className=' hover:underline'>
-              NotionNext {siteConfig('VERSION')}
+              BARU LOGISTIC {siteConfig('2026')}
             </a>
           </span>
         </div>
