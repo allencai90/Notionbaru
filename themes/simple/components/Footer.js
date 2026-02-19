6642899fdcadad42d1057d7ad14b0fd58285496b
allencai90 <cyychad@gmail.com>
2026-02-19 12:01:04 +0800

    Update Footer.js

--- themes/simple/components/Footer.js
+++ themes/simple/components/Footer.js
@@ -2,32 +2,22 @@ import { BeiAnGongAn } from '@/components/BeiAnGongAn'
 import DarkModeButton from '@/components/DarkModeButton'
 import { siteConfig } from '@/lib/config'
 
-/**
- * 页脚
- * @param {*} props
- * @returns
- */
 export default function Footer(props) {
   const d = new Date()
   const currentYear = d.getFullYear()
   const since = siteConfig('SINCE')
   const copyrightDate =
     parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
-
   return (
     <footer className='relative w-full bg-black px-6 border-t'>
       <DarkModeButton className='text-center pt-4' />
-
       <div className='text-yellow-300 container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm'>
         <div className='text-center'>
-          &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}. All rights
-          reserved.
+          &copy; {copyrightDate} {siteConfig('AUTHOR')}. All rights reserved.
         </div>
         <div className='md:p-0 text-center md:text-right text-xs'>
-          {/* 右侧链接 */}
-          {/* <a href="#" className="text-black no-underline hover:underline">Privacy Policy</a> */}
           {siteConfig('BEI_AN') && (
-            <a
+            
               href={siteConfig('BEI_AN_LINK')}
               className='no-underline hover:underline ml-4'>
               {siteConfig('BEI_AN')}
@@ -35,11 +25,9 @@ export default function Footer(props) {
           )}
           <BeiAnGongAn />
           <span className='no-underline ml-4'>
-            Powered by
-            <a
-              href='https://uslogs.com'
-              className=' hover:underline'>
-              BARU LOGISTIC {siteConfig('2026')}
+            Powered by{' '}
+            <a href='https://uslogs.com' className='hover:underline'>
+              BARU LOGISTIC 2026
             </a>
           </span>
         </div>
